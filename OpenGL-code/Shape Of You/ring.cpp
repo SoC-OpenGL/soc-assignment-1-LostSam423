0f7c50e9ed0528b0a103ec415283539f34f45488
LostSam423 <beherasambit423@gmail.com>
2020-03-27 23:50:57 +0530

    Solution To First Qsn

---
+++ OpenGL-code/Shape Of You/ring.cpp
@@ -0,0 +1,125 @@
+
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <cmath>
+
+#include "shader.hpp"
+#include "readfile.hpp"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+void drawRing(GLfloat *vertices, GLuint *indices, int N);
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 800;
+const int N = 360;
+
+ 
+int main()
+{   
+    GLfloat vertices[N*3];
+    GLuint indices[N*2];
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Soviet Republic", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    glewExperimental = GL_TRUE;
+    glewInit();
+    
+    glViewport(0, 0,SCR_WIDTH, SCR_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    drawRing(vertices, indices, N);
+    Shader *shdr = new Shader("shad.vs","shad.fs");    
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers( 1, &VBO );
+    
+    
+    glBindVertexArray( VAO );
+
+    glBindBuffer( GL_ARRAY_BUFFER, VBO );
+    glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3* sizeof( GLfloat ), (GLvoid * ) 0 );
+    glEnableVertexAttribArray( 0 );
+    
+    glGenBuffers( 1, &EBO);
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        
+        processInput(window);
+
+        glfwPollEvents();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        shdr->use();
+
+        glBindVertexArray( VAO );
+
+        glDrawArrays(GL_LINE_LOOP, 0, N-1);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays( 1, &VAO );
+    glDeleteBuffers(1, &VBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void drawRing(GLfloat *vertices, GLuint *indices, int N)
+{
+    GLfloat centre = 0.0f;
+    GLfloat rad = 0.2f;
+    float i = 0;
+
+    while(i<=N){
+        GLfloat x = centre + rad * cos(i*M_PI/180);
+        GLfloat y = centre + rad * sin(i*M_PI/180);
+        vertices[int(i*3)] = x;
+        vertices[int(i*3+1)] = y;
+        vertices[int(i*3+2)] = 0.0f;
+        i+= (360/N);
+    }
+
+    for (int i = 0; i < N; ++i)
+    {
+        indices[i*2] = i;
+        indices[i*2+1] = i+1;
+    }
+    indices[N*2-1] = 0;
+}
+
+
